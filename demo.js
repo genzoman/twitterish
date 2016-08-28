2c48825f72dd211f57c0f08f30ac8e82f4b4505e
darrin <darrinthomascecil@gmail.com>
2016-08-28 17:59:08 -0400

    woot

--- demo.js
+++ demo.js
@@ -11,28 +11,29 @@ getDom("https://dev.twitter.com/rest/reference/get/statuses/mentions_timeline", 
 
   })
   .then(data => {
-    return data.map(e => {
-      return () => {
-        return getDom(baseUrl + e, [".leaf", ".param"])
-          .then(data => {
-            let params = getParametersForEndpoint(data, ".param");
-            return {
-              url: baseUrl+ e,
-              params: params
-            }
-          })
-      }
-
-    });
-
-  })
-  .then(promises => {
-    let things = promises.map(e =>{
-      return e();
-    });
-    Promise.all(things)
+    var promises = getParametersPromises(data);
+    promises = promises.map(e => e())
+    Promise.all(promises)
       .then(data => {
+        console.log("done")
         debugger;
       })
-    
+
+  })
+
+
+function getParametersPromises(data) {
+  return data.map(e => {
+    return () => {
+      return getDom(baseUrl + e, [".leaf", ".param"])
+        .then(data => {
+          let params = getParametersForEndpoint(data, ".param");
+          return {
+            url: baseUrl + e,
+            params: params
+          }
+        })
+    }
+
   });
+}
