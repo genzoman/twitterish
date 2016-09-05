db98a4ef3563b8b843a71fa159cbc39e99bf5fbb
darrin <darrinthomascecil@gmail.com>
2016-09-05 13:28:23 -0400

    ship it!

--- tree.js
+++ tree.js
@@ -10,19 +10,19 @@ json = (path) => {
         });
     });
 }
-var ul = d3.select("body").append("ul");
+var container = d3.select("body").append("div").attr("id","container");
 let toggleChildren = (e) => {
   // var children = e.children;
   // render(children);
 }
-let render = (data,isClicked) => {
+let render = (data,parent,isClicked) => {
    
    var hierarchy = !isClicked ? d3.hierarchy(data): data;
    if(hierarchy.parent === null) {
-      let li = ul.selectAll("li.node")
+      let divs = parent.selectAll("div")
     .data([hierarchy])
     .enter()
-      .append("li")
+      .append("div")
       .text(d => {
         d.hasChildren = d.children && d.children.length > 0
         return d.data.name;
@@ -32,10 +32,10 @@ let render = (data,isClicked) => {
    
   }
   else {
-    let li = ul.selectAll("li .node")
+    let divs = parent.selectAll(".node")
       .data(data)
       .enter()
-        .append("li")
+        .append("div")
         .text(d => {
           return d.data.name;
         })
@@ -44,25 +44,26 @@ let render = (data,isClicked) => {
         .attr("class","children");
       
   }
- 
+  events();
 }
 var shouldRemove = false;
 json("./flare.json")
   .then(data =>{
-    render(data,false); 
-    events();
+    render(data,container,false); 
+    //events();
   });
 
  let events = () => {
    d3.selectAll(".children, .root")
   .on("click",function(e) {
+    d3.event.stopPropagation();
     if(e.isOpen){
-      d3.selectAll(".children").remove();
+      d3.select(this).selectAll(".children").remove();
        e.isOpen = !e.isOpen;
       return;
     }
     e.isOpen = !e.isOpen;
     //toggleChildren(e);
-    render(e.children,true)
+    render(e.children,d3.select(this),true)
   });
  }
